c4789c11df236b67d1a12ede6ea410ebe8a25498
azuma hozumi <e1n21006@oit.ac.jp>
2024-07-08 11:58:56 +0900

    add semonster2 change

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ package org.example;
 
 public class App {
   public String getGreeting() {
-    return "こんにちは SEMonster";
+    return "hello SEMonster";
   }
 
   public static void main(String[] args) {
