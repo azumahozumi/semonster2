b77a731ec56ae13f891178caecdf560e44c8f9cf
azuma hozumi <e1n21006@oit.ac.jp>
2024-07-21 12:09:50 +0900

    kaihatu2

--- app/src/main/java/org/example/Player.java
+++ app/src/main/java/org/example/Player.java
@@ -4,20 +4,25 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Player {
-  String name;// Player name
-  ArrayList<Monster> monsterDeck = new ArrayList<>();
-  LinkedList<Integer> randomNumberList;// randomな0~4の数値を必要な数保存しておく
+  String name; // プレイヤーの名前
+  ArrayList<Monster> monsterDeck = new ArrayList<>(); // 8体のモンスターを持つデッキ
+  LinkedList<Integer> randomNumberList; // ランダムな0~4の数値リスト
+  int hp; // プレイヤーのHP
 
-  Player(LinkedList<Integer> randomNumberList, String playerName) {
+  // コンストラクタでHPの初期値も設定
+  Player(LinkedList<Integer> randomNumberList, String playerName, int initialHP) {
     this.randomNumberList = randomNumberList;
     this.name = playerName;
+    this.hp = initialHP;
+    drawMonsters(); // プレイヤー作成時にモンスターを8体ドロー
   }
 
   /**
-   * モンスターを5体分drawする．利用するランダムな値(0~4)はrandomNumberListから取得する
+   * モンスターを8体分ドローする．
+   * 利用するランダムな値(0~4)はrandomNumberListから取得する
    */
   public void drawMonsters() {
-    for (int i = 0; i < 5; i++) {
+    while (this.randomNumberList.size() >= 2 && this.monsterDeck.size() < 8) {
       this.monsterDeck.add(new Monster(this.randomNumberList.pop(), this.randomNumberList.pop()));
     }
   }
@@ -25,11 +30,12 @@ public class Player {
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    sb.append("Deck:" + this.name + "\n");
+    sb.append("Player: " + this.name + "\n");
+    sb.append("HP: " + this.hp + "\n");
+    sb.append("Deck:\n");
     for (Monster m : this.monsterDeck) {
       sb.append(m);
     }
     return sb.toString();
   }
-
 }
